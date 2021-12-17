e921f2d0b2feb8100a922a53079472c909fb34dc
YunKeon112 <kyun@pentasecurity.com>
2021-12-17 15:51:14 +0900

    [func] add testHandleAuth

--- sdp_agent/test/lib/Storage/storage_test.go
+++ sdp_agent/test/lib/Storage/storage_test.go
@@ -20,3 +20,49 @@ const (
 	testId    = "eeee"
 	testMeta  = `{"owner":"2f2f"}`
 )
+
+func testHandleAuth(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte(`{"error":"Expected POST method"}`))
+		return
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"error":"empty request body"}`))
+		return
+	}
+
+	// same as AuthBody but, change each field as a pointer
+	var authBody struct {
+		User      *string          `json:"user"`
+		Operation *json.RawMessage `json:"operation"`
+	}
+	err = json.Unmarshal(body, &authBody)
+	if err != nil || authBody.User == nil || authBody.Operation == nil {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"error":"malformed request body"}`))
+		return
+	}
+	var opReq struct {
+	}
+	err = json.Unmarshal(*authBody.Operation, &opReq)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"error":"malformed request body"}`))
+		return
+	}
+
+	res := struct {
+		Token string `json:"token"`
+	}{testToken}
+	fmt.Println("res", res)
+	rsp, err := json.Marshal(res)
+	fmt.Println("rsp", string(rsp))
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(`{"error":"internal error: unable to marshal json"}`))
+	}
+	w.Write(rsp)
+}
