e8a6a22ba408c3d356d1b470469bf611238a4822
YunKeon112 <kyun@pentasecurity.com>
2021-12-17 14:42:13 +0900

    [func] add generateECDSAKey

--- sdp_agent/test/lib/Key/Key.go
+++ sdp_agent/test/lib/Key/Key.go
@@ -23,3 +23,21 @@ type KeyEntry struct {
 	PrivKey   []byte `json:"priv_key"`
 	Encrypted bool   `json:"encrypted"`
 }
+
+func generateECDSAKey(seed string) (privKey *ecdsa.PrivateKey, err error) {
+	if len(seed) > 0 {
+		b := sha256.Sum256([]byte(seed))
+		privKey = new(ecdsa.PrivateKey)
+		privKey.D = new(big.Int).SetBytes(b[:])
+		X, Y := c.ScalarBaseMult(b[:])
+		privKey.PublicKey = ecdsa.PublicKey{
+			Curve: c,
+			X:     X,
+			Y:     Y,
+		}
+	} else {
+		privKey, err = ecdsa.GenerateKey(c, rand.Reader)
+	}
+
+	return privKey, err
+}
