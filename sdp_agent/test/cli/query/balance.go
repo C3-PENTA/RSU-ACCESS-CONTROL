9637750e45139e87432310bdfbe75e652d20f7ba
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:32:34 +0900

    [func] add balanceFunc

--- sdp_agent/test/cli/query/balance.go
+++ sdp_agent/test/cli/query/balance.go
@@ -16,3 +16,39 @@ var BalanceCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  balanceFunc,
 }
+
+func balanceFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
+
+	udc, err := cmd.Flags().GetUint32("udc")
+	if err != nil {
+		return err
+	}
+
+	// TODO: do some sanity check on client side
+	res, err := rpc.QueryBalance(udc, args[0])
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	var balance types.Currency
+	err = json.Unmarshal([]byte(res), &balance)
+	if err != nil {
+		return err
+	}
+	fmt.Println(balance.String())
+
+	return nil
+}
