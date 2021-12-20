61de76d3a68caf31fbe47a290d9f596ddc366d0b
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:34:22 +0900

    [func] add udcLockFunc

--- sdp_agent/test/cli/query/udc_lock.go
+++ sdp_agent/test/cli/query/udc_lock.go
@@ -16,3 +16,34 @@ var UdcLockCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(2),
 	RunE:  udcLockFunc,
 }
+
+func udcLockFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
+
+	res, err := rpc.QueryUDCLock(args[0], args[1])
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	var amount types.Currency
+	err = json.Unmarshal(res, &amount)
+	if err != nil {
+		return err
+	}
+	fmt.Println(amount.String())
+
+	return nil
+
+}
