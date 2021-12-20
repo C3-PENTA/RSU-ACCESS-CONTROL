f6f242d835349896f345f171c368555ccdb9f05b
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:31:25 +0900

    [func] add stakeFunc

--- sdp_agent/test/cli/query/stake.go
+++ sdp_agent/test/cli/query/stake.go
@@ -18,3 +18,51 @@ var StakeCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  stakeFunc,
 }
+
+func stakeFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
+
+	res, err := rpc.QueryStake(args[0])
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	if res == nil || len(res) == 0 || string(res) == "null" {
+		fmt.Println("no stake")
+	} else {
+		var stake types.Stake
+		err = json.Unmarshal(res, &stake)
+		if err != nil {
+			return err
+		}
+
+		valb64str := base64.StdEncoding.EncodeToString(stake.Validator)
+		valb64, err := hex.DecodeString(valb64str)
+		if err != nil {
+			return err
+		}
+		valb64b64str := base64.StdEncoding.EncodeToString(valb64)
+
+		fmt.Printf("amount: %s\n", stake.Amount.String())
+		fmt.Printf("validator pubkey (hex)   : 0x%s\n", valb64str)
+		fmt.Printf("validator pubkey (base64): %s\n", valb64b64str)
+		for i, d := range stake.Delegates {
+			fmt.Printf("  delegate %2d: %s from %s\n",
+				i+1, d.Amount.String(), d.Delegator)
+		}
+	}
+
+	return nil
+}
