3f2a3469ff08ebaea094a68810487d9fe487a8e4
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:08:04 +0900

    [func] add appversion Func

--- sdp_agent/test/cli/query/app_version.go
+++ sdp_agent/test/cli/query/app_version.go
@@ -16,3 +16,47 @@ var AppVersionCmd = &cobra.Command{
 	Short: "App and protocol versions",
 	RunE:  appVersionFunc,
 }
+
+func appVersionFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	res, err := rpc.QueryAppVersion()
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	var appVersion struct {
+		AppVersion           string   `json:"app_version,omitempty"`
+		AppProtocolVersions  []uint64 `json:"app_protocol_versions,omitempty"`
+		StateProtocolVersion uint64   `json:"state_protocol_version,omitempty"`
+		AppProtocolVersion   uint64   `json:"app_protocol_version,omitempty"`
+	}
+	err = json.Unmarshal([]byte(res), &appVersion)
+	if err != nil {
+		return err
+	}
+
+	var vers []string
+	for _, v := range appVersion.AppProtocolVersions {
+		vers = append(vers, strconv.FormatUint(v, 10))
+	}
+	versString := strings.Join(vers, ", ")
+
+	fmt.Println("App version                    =",
+		appVersion.AppVersion)
+	fmt.Println("Supported protocol versions    =", "[", versString, "]")
+	fmt.Println("Current state protocol version =",
+		appVersion.StateProtocolVersion)
+	fmt.Println("Current app protocol version   =",
+		appVersion.AppProtocolVersion)
+
+	return nil
+}
