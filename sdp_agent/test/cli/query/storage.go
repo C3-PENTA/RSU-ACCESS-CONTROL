442121cc20d812e42e06d58124d183a39a11b490
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:07:11 +0900

    [func] add storageFunc

--- sdp_agent/test/cli/query/storage.go
+++ sdp_agent/test/cli/query/storage.go
@@ -16,3 +16,43 @@ var StorageCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  storageFunc,
 }
+
+func storageFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
+
+	res, err := rpc.QueryStorage(args[0])
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	if res == nil || len(res) == 0 || string(res) == "null" {
+		fmt.Println("no storage")
+		return nil
+	}
+
+	var storage types.Storage
+	err = json.Unmarshal(res, &storage)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("owner: %s\n", storage.Owner)
+	fmt.Printf("url: %s\n", storage.Url)
+	fmt.Printf("registration_fee: %s\n", storage.RegistrationFee.String())
+	fmt.Printf("hosting_fee: %s\n", storage.HostingFee.String())
+	fmt.Printf("active: %t\n", storage.Active)
+
+	return nil
+}
