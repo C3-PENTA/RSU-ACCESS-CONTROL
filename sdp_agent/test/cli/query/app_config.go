1a5e6fcdee962d597dbbff28a2276f7a5cc1fed0
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:30:39 +0900

    [func] add app_config

--- sdp_agent/test/cli/query/app_config.go
+++ sdp_agent/test/cli/query/app_config.go
@@ -16,3 +16,33 @@ var UdcCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  udcFunc,
 }
+
+func appConfigFunc(cmd *cobra.Command, args []string) error {
+	// TODO: do some sanity check on client side
+	res, err := rpc.QueryAppConfig()
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	var appConfig types.AMOAppConfig
+	err = json.Unmarshal([]byte(res), &appConfig)
+	if err != nil {
+		return err
+	}
+
+	cfg, err := config.GetConfig(util.DefaultConfigFilePath())
+	if err != nil {
+		return err
+	}
+
+	cfg.SetABCIConfig(appConfig)
+	cfg.Save()
+
+	fmt.Println(string(res))
+
+	return nil
+}
