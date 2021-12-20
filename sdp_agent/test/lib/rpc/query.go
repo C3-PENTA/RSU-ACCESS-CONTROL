714f69cd7e87825901ea4b42c71f9eb960e5b76c
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 09:41:03 +0900

    [func] add QueryBalance

--- sdp_agent/test/lib/rpc/query.go
+++ sdp_agent/test/lib/rpc/query.go
@@ -9,3 +9,16 @@ func QueryAppConfig() ([]byte, error) {
 	ret, err := ABCIQuery("/config", nil)
 	return ret, err
 }
+
+func QueryBalance(udc uint32, address string) ([]byte, error) {
+	queryPath := "/balance"
+	if udc != 0 {
+		queryPath = fmt.Sprintf("%s/%d", queryPath, udc)
+	}
+	address = toUpper(address)
+	ret, err := ABCIQuery(queryPath, address)
+	if ret == nil {
+		ret = []byte("0")
+	}
+	return ret, err
+}
