2e40c6f2541fd0efad08fc0f5cf11542f1c7bd48
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:06:52 +0900

    fix test storage

--- sdp_agent/test/cli/query/storage.go
+++ sdp_agent/test/cli/query/storage.go
@@ -1,1 +1,18 @@
-test
+package query
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/amolabs/amo-client-go/lib/rpc"
+	"github.com/amolabs/amo-client-go/lib/types"
+)
+
+var StorageCmd = &cobra.Command{
+	Use:   "storage <storage_id>",
+	Short: "Storage info",
+	Args:  cobra.MinimumNArgs(1),
+	RunE:  storageFunc,
+}
