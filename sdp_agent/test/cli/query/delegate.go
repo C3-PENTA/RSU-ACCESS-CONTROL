f85d04759faf9bec8316d8ac1009ccaa22b394ab
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 10:33:34 +0900

    [func] add delegateFunc

--- sdp_agent/test/cli/query/delegate.go
+++ sdp_agent/test/cli/query/delegate.go
@@ -16,3 +16,38 @@ var DelegateCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  delegateFunc,
 }
+
+func delegateFunc(cmd *cobra.Command, args []string) error {
+	asJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
+
+	res, err := rpc.QueryDelegate(args[0])
+	if err != nil {
+		return err
+	}
+
+	if rpc.DryRun {
+		return nil
+	}
+
+	if asJson {
+		fmt.Println(string(res))
+		return nil
+	}
+
+	if res == nil || len(res) == 0 || string(res) == "null" {
+		fmt.Println("no delegate")
+	} else {
+		var delegate types.Delegate
+		err = json.Unmarshal(res, &delegate)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("delegatee address: %s\namount: %s\n",
+			delegate.Delegatee, delegate.Amount.String())
+	}
+
+	return nil
+}
